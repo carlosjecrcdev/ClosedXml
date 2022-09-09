8781abdaa1ad2769d58f95255a2ebcc0197708f6
CarlosCruz <carlosjecrc@gmail.com>
2022-09-08 22:15:37 -0500

    Agregar archivos de proyecto.

---
+++ ClosedXml/XlsxFile.cs
@@ -0,0 +1,30 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosedXml
+{
+    internal class XlsxFile
+    {
+        public static void GenerarArchivoExcel()
+        {
+            var ruta = ConfigurationManager.AppSettings["rutaExcel"];
+
+            if(ruta== null)
+            {
+                throw new Exception("No se encontró ruta para guardar el fichero.");
+            }
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Mi primer hoja");
+                worksheet.Cells("A1").Value = "Hello CoderAnonimo";
+                workbook.SaveAs(ruta);
+            }
+        }
+    }
+}
